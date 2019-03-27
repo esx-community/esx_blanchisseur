cb2e7576058eb0eb4fe461c4ab75ec9461209adc
BackrndSource <back.rndsource@gmail.com>
2019-03-27 18:36:16 +0000

    Create es.lua

---
+++ locales/es.lua
@@ -0,0 +1,13 @@
+Locales['es'] = {
+	['blanchisseurblip'] 	= 'blanqueo de dinero',
+	['press_menu'] 			= 'pulsa ~INPUT_CONTEXT~ para lavar tu ~y~dinero negro',
+	['Nothere'] 			= 'vuelve más tarde...',
+	['Whitening'] 			= 'lavado en proceso...',
+	['Nocash'] 				= 'no tienes suficiente dinero para blanquear, mínimo: ',
+	['cash'] 				= 'has recivido: ~r~',
+	['cash1'] 				= '€ blanqueados',
+	['Notification'] 		= 'blanqueo de dinero',
+	['wash'] 				= 'blanquea tu dinero negro',
+	['wash_money_amount']   = 'cantidad para blanquear',
+	['invalid_amount']      = 'cantidad no válida',
+}
