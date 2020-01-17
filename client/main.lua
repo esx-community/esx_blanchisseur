a9b6d4ecccf464fccd71ebef271be658b0dd51a4
rex2630 <rex2630@gmail.com>
2020-01-17 21:30:15 +0100

    Minor cleanup

--- client/main.lua
+++ client/main.lua
@@ -1,23 +1,8 @@
-local Keys = {
-	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, 
-	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, 
-	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, 
-	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}
-
-ESX                           = nil
-local GUI					  = {}
+ESX = nil
+local GUI, CurrentActionData = {}, {}
 local HasAlreadyEnteredMarker = false
-local LastZone                = nil
-GUI.Time           			  = 0
-local CurrentAction           = nil
-local CurrentActionMsg        = ''
-local CurrentActionData       = {}
+local LastZone, CurrentAction, CurrentActionMsg
+GUI.Time = 0
 
 Citizen.CreateThread(function()
 	while ESX == nil do
@@ -38,30 +23,22 @@ AddEventHandler("esx_blanchisseur:notify", function(icon, type, sender, title, t
 end)
 
 function OpenBlanchisseurMenu()
-
   local elements = { }
 	  table.insert(elements, {label = _U('wash'),    value = 'wash_money'})
 
 	ESX.UI.Menu.CloseAll()
 
-    ESX.UI.Menu.Open(
-      'default', GetCurrentResourceName(), 'whitening',
-      {
+    ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'whitening', {
         title    = _U('Notification'),
         align    = 'top-left',
         elements = elements,
-        },
-
-        function(data, menu)
+        }, function(data, menu)
 		
 		if data.current.value == 'wash_money' then
 
-			ESX.UI.Menu.Open(
-				'dialog', GetCurrentResourceName(), 'wash_money_amount_',
-				{
+			ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'wash_money_amount_', {
 					title = _U('wash_money_amount')
-				},
-				function(data, menu)
+				}, function(data, menu)
 
 					local amount = tonumber(data.value)
 
@@ -73,24 +50,17 @@ function OpenBlanchisseurMenu()
 						TriggerServerEvent('esx_blanchisseur:washMoney', amount)
 						ESX.UI.Menu.CloseAll()
 					end
-
-				end,
-				function(data, menu)
+				end, function(data, menu)
 					menu.close()
-				end
-			)
-
+				end)
 			end
-    end,
-    function(data, menu)
+    end, function(data, menu)
 
       menu.close()
 
       CurrentAction     = 'whitening'
       CurrentActionData = { }
-    end
-    )
-
+    end)
 end
 
 AddEventHandler('esx_blanchisseur:hasEnteredMarker', function(zone)
@@ -184,7 +154,6 @@ Citizen.CreateThread(function()
 
         CurrentAction = nil
       end
-
     end
   end
 end)
